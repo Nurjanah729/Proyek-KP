b0d987a0a6888da047082b50e7cfb3a5ae238754
Siti Nurjanah <siti.n6969294@gmail.com>
2026-01-21 15:55:53 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,19 +1,17 @@
-import mysql.connector
 import streamlit as st
+import mysql.connector
 
 def get_db():
-    return mysql.connector.connect(
-        host=st.secrets["mysql"]["host"],
-        port=st.secrets["mysql"]["port"],
-        database=st.secrets["mysql"]["database"],
-        user=st.secrets["mysql"]["user"],
-        password=st.secrets["mysql"]["password"],
-        ssl_ca=st.secrets["mysql"]["ssl_ca"],
-        ssl_verify_cert=True,
-        use_pure=True,
-        connection_timeout=5
+    db = st.secrets["mysql"]
+
+    conn = mysql.connector.connect(
+        host=db["host"],
+        port=db["port"],
+        user=db["user"],
+        password=db["password"],
+        database=db["database"],
+        ssl_disabled=True   # ⬅️ KUNCI UTAMA
     )
 
-    
-
+    return conn
 
