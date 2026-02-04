6a259bfebeb655a0ffe8e599422f896f956428a5
Siti Nurjanah <siti.n6969294@gmail.com>
2026-02-04 08:23:45 +0700

    Update mahasiswa_dashboard.py

--- mahasiswa_dashboard.py
+++ mahasiswa_dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from db import get_db
-from pengaturan import pengaturan_page  # import fungsi pengaturan
+from pengaturan import pengaturan  # import fungsi pengaturan
 
 
 def mahasiswa_dashboard(student_id):
@@ -232,6 +232,7 @@ def mahasiswa_dashboard(student_id):
     # ======================
     st.markdown("### 📋 Detail Nilai Modul")
     st.dataframe(df, use_container_width=True, hide_index=True)
+
 
 
 
