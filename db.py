2776933f674a31aef9fdf6661baaf6108e4a5946
Siti Nurjanah <siti.n6969294@gmail.com>
2026-02-05 22:25:18 +0700

    Update db.py

--- db.py
+++ db.py
@@ -14,4 +14,21 @@ def get_db():
     )
 
     return conn
+    
+def get_latest_prediction(student_id):
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT result
+        FROM predictions
+        WHERE student_id = %s
+        ORDER BY id DESC
+        LIMIT 1
+    """, (student_id,))
+
+    data = cursor.fetchone()
+    conn.close()
+    return data
+
 
